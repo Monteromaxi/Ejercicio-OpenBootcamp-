b5bd6afc24f5c1fb67ce67d1cdcf6005036133d1
Monteromaxi <monteromaxi@gmail.com>
2022-12-08 16:00:35 -0300

    Ejercicios

---
+++ Ejercicio2.java
@@ -0,0 +1,17 @@
+public class Main{
+
+    public static void main(String[] args) {
+        Coche miCoche = new Coche();
+        miCoche.MasPuertas();
+        System.out.println(miCoche.puertas);
+    }
+
+}
+
+class Coche {
+    public int puertas = 4;
+
+    public void MasPuertas() {
+        this.puertas++;
+    }
+}
